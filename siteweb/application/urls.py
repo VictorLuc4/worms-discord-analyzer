3aad90a5082cec09449dc7ae8377e8daa0f2264d
vlucas <vlucas@online.net>
2020-04-16 14:55:57 +0200

    add enpoint post + get servers

--- siteweb/application/urls.py
+++ siteweb/application/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.worm, name='worm'),
+    path('servers/', views.servers, name='servers'),
+    path('server/', views.server, name='server'),
 ]
--- siteweb/application/views.py
+++ siteweb/application/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
+from .models import Person, Server, Message
 
 # Create your views here.
 def worm(request):
     return HttpResponse("Hello, world. You're at the worm index.")
+
+
+@csrf_exempt 
+def servers(request):
+    if request.method == 'GET':
+        server_list = Server.objects.order_by('name')
+        print(server_list[0])
+        return HttpResponse(server_list)
+    else:
+        return HttpResponse("KO")
+
+
+@csrf_exempt 
+def server(request):    
+    if request.method == 'POST':
+        print(request.POST)
+        name = request.POST.get('name')
+        code =request.POST.get('code')
+        tmp_srv = Server(name=name, code=code)
+        tmp_srv.save()
+        return HttpResponse("Server %s saved in DB", name)
+    else:
+        return HttpResponse("KO")
+        
