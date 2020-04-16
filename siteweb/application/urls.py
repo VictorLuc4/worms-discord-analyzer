566b7253b4d2cb5dd9cc7acae497f6701460b9a0
vlucas <vlucas@online.net>
2020-04-16 16:51:55 +0200

    add API endpoint for a person

--- siteweb/application/urls.py
+++ siteweb/application/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.worm, name='worm'),
     path('servers/', views.servers, name='servers'),
     path('server/', views.server, name='server'),
+
+    path('persons/', views.persons, name='persons'),
+    path('person/', views.person, name='person'),
 ]
--- siteweb/application/views.py
+++ siteweb/application/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
-from .models import Person, Server, Message
+from .models import Server, Person, Message
 
 # Create your views here.
 def worm(request):
@@ -13,7 +13,6 @@ def worm(request):
 def servers(request):
     if request.method == 'GET':
         server_list = Server.objects.order_by('name')
-        print(server_list[0])
         return HttpResponse(server_list)
     else:
         return HttpResponse("KO")
@@ -22,12 +21,63 @@ def servers(request):
 @csrf_exempt 
 def server(request):    
     if request.method == 'POST':
-        print(request.POST)
         name = request.POST.get('name')
         code =request.POST.get('code')
-        tmp_srv = Server(name=name, code=code)
-        tmp_srv.save()
-        return HttpResponse("Server %s saved in DB", name)
+
+        serv = Server.objetcs.get(code=code)
+        if serv:    # Update the name
+            serv.name = name
+            serv.save()
+        else:       # Create a new Server
+            serv = Server(name=name, code=code)
+            serv.save()
+        return HttpResponse("OK")
     else:
         return HttpResponse("KO")
+
+#############################
+
+@csrf_exempt 
+def persons(request):
+    if request.method == 'GET':
+        allpersons = Person.objects.order_by('username')
+        persons = []
+        for p in allpersons:
+            persons.append(p)
+            persons.append(' ')
+        return HttpResponse(persons)        
+
+    else:
+        return HttpResponse("KO")
+
+@csrf_exempt 
+def person(request):   
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        code = request.POST.get('code')
+        servername = request.POST.get('servername')
+
         
+        try:
+            server = Server.objects.get(name=servername)    
+        except Server.DoesNotExist:
+            server = None
+
+        try:
+            man = Person.objects.get(code=code)
+        except Person.DoesNotExist:
+            man = None
+
+        if man:     # Update the name
+            man.username = username
+            man.server.add(server)
+            man.save()
+        else:       # Create a new Server
+            man = Person(username=username, code=code)
+            man.save()
+            man.server.add(server)
+            man.save()
+        
+        return HttpResponse("OK")
+    else:
+        return HttpResponse("KO")
